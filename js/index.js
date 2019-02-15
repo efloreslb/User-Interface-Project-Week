bfc28cf73a08cb8924551738e397586e427011ac
Edgar F <eflores.ef@gmail.com>
2019-02-14 20:15:17 -0800

    Finished services tabs JS functionality.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // JS goes here
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.tabContent = document.querySelector(`.tab-content[data-tab="${this.data}"]`);
+        this.tabContent = new TabContent(this.tabContent);
+        this.element.addEventListener('click', this.tabSelect.bind(this));
+    }
+
+    tabSelect() {
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach(tab => tab.classList.remove('tab-selected'));
+        this.element.classList.add('tab-selected');
+        this.tabContent.contentSelect();
+    }
+}
+
+class TabContent {
+    constructor(element) {
+        this.element = element;
+    }
+
+    contentSelect() {
+        const contents = document.querySelectorAll('.tab-content');
+        contents.forEach(content => content.classList.remove('tab-content-selected'));
+        this.element.classList.add('tab-content-selected');
+    }
+}
+
+const tabs = document.querySelectorAll('.tab-section .tab')
+                     .forEach(tab => new TabLink(tab));
