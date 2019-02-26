a178a20abd790a630eb0cc4b13fa16d31656b7dc
Edgar F <eflores.ef@gmail.com>
2019-02-25 20:13:40 -0800

    Replaced carousel with new reusable JS carousel component in Projects page.

--- js/carousel.js
+++ js/carousel.js
@@ -1,86 +1,56 @@
-// Carousel Component
-let carouselStatus = 1;
-let carouselTimer = 4000;
+// // Carousel Component
 
-window.onload = () => {
-    carouselLoop();
-}
-
-let startCarouselLoop = setInterval(() => {
-    carouselLoop();
-}, carouselTimer);
-
-document.querySelector("#carousel-container").onmouseenter = () => {
-    clearInterval(startCarouselLoop);
-}
-
-document.querySelector("#carousel-container").onmouseleave = () => {
-    startCarouselLoop = setInterval(() => {
-        carouselLoop();
-    }, carouselTimer);
-}
-
-document.querySelector(".carousel-button-previous").onclick = () => {
-    if(carouselStatus === 1) {
-        carouselStatus = 2;
-    } else if (carouselStatus === 2) {
-        carouselStatus = 3;
-    } else if(carouselStatus === 3) {
-        carouselStatus = 1;
-    }
-    carouselLoop();
-}
-
-document.querySelector(".carousel-button-next").onclick = () => {
-    carouselLoop();
-}
-
-let carouselLoop = () => {
-    if(carouselStatus === 1) {
-        document.querySelector("#carousel-item02").style.opacity = '0';
-        setTimeout(() => {
-            document.querySelector('#carousel-item01').style.right = "0px";
-            //document.querySelector('#carousel-item01').style.zIndex = "100";
-            document.querySelector('#carousel-item02').style.right = "-800px";
-            //document.querySelector('#carousel-item02').style.zIndex = "150";
-            document.querySelector('#carousel-item03').style.right = "800px";
-            //document.querySelector('#carousel-item03').style.zIndex = "50";
-        }, 100);
-        setTimeout(() => {
-            document.querySelector("#carousel-item02").style.opacity = '1';
-        }, 1000)
-        carouselStatus = 2;
+class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.items = document.querySelectorAll('.carousel-item');
+        this.nextBtn = document.querySelector(".carousel-button-next");
+        this.prevBtn = document.querySelector(".carousel-button-prev");
+        this.counter = 0;
+        this.current = this.items[this.counter];
+        this.nextBtn.addEventListener('click', this.nextItem.bind(this));
+        this.prevBtn.addEventListener('click', this.prevItem.bind(this));
     }
 
-    else if(carouselStatus === 2) {
-        document.querySelector("#carousel-item03").style.opacity = '0';
-        setTimeout(() => {
-            document.querySelector('#carousel-item02').style.right = "0px";
-            //document.querySelector('#carousel-item02').style.zIndex = "100";
-            document.querySelector('#carousel-item03').style.right = "-800px";
-            //document.querySelector('#carousel-item03').style.zIndex = "150";
-            document.querySelector('#carousel-item01').style.right = "800px";
-            //document.querySelector('#carousel-item01').style.zIndex = "50";
-        }, 100);
-        setTimeout(() => {
-            document.querySelector("#carousel-item03").style.opacity = '1';
-        }, 1000)
-        carouselStatus = 3;
+    prevItem() {
+        if (this.counter === 0) {
+            console.log(`if ${this.counter}`);
+            this.counter = this.items.length - 1;
+            this.items.forEach(item => {
+                item.classList.remove('carousel-current');
+            });
+            this.current = this.items[this.counter];
+            this.current.classList.add('carousel-current');
+        }
+        else {
+            console.log(`else ${this.counter}`);
+            this.counter = this.counter - 1;
+            this.items.forEach(item => {
+                item.classList.remove('carousel-current');
+            });
+            this.current = this.items[this.counter];
+            this.current.classList.add('carousel-current');
+        }
     }
 
-    else if(carouselStatus === 3) {
-        document.querySelector("#carousel-item01").style.opacity = '0';
-        setTimeout(() => {
-            document.querySelector('#carousel-item03').style.right = "0px";
-            //document.querySelector('#carousel-item03').style.zIndex = "100";
-            document.querySelector('#carousel-item01').style.right = "-800px";
-            //document.querySelector('#carousel-item01').style.zIndex = "150";
-            document.querySelector('#carousel-item02').style.right = "800px";
-            //document.querySelector('#carousel-item02').style.zIndex = "50";
-        }, 100);
-        setTimeout(() => {
-            document.querySelector("#carousel-item01").style.opacity = '1';
-        }, 1000)
-        carouselStatus = 1;
+    nextItem() {
+        if (this.counter >= this.items.length-1) {
+            this.counter = 0;
+            this.items.forEach(item => {
+                item.classList.remove('carousel-current');
+            })
+            this.current = this.items[this.counter];
+            this.current.classList.add('carousel-current');
+        }
+        else {
+            this.items.forEach(item => {
+                item.classList.remove('carousel-current');
+            })
+            this.counter++;
+            this.current = this.items[this.counter];
+            this.current.classList.add('carousel-current');
+        }
     }
 }
+
+const carousel = new Carousel(document.querySelector('.carousel-container'));
